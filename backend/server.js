50db953425d528d32962b13dc9000b27f0c2a56c
Hipe Gelo <roland@hipe.asia>
2026-01-17 18:55:10 +0800

    feat: created other endpoints

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,18 @@ app.get('/api/notes', (req, res) => {
   res.send('You got notes!');
 });
 
+app.post('api/notes', (req, res) => {
+  res.status(201).json({ message: 'Note created' });
+});
+
+app.put('api/notes/:id', (req, res) => {
+  res.status(201).json({ message: 'Note updated' });
+});
+
+app.delete('api/notes/:id', (req, res) => {
+  res.status(201).json({ message: 'Note deleted' });
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
