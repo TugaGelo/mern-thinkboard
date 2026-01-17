f6286f5acb6253da7e3e6adf87f6a65c7686ceb4
Hipe Gelo <roland@hipe.asia>
2026-01-17 20:15:49 +0800

    feat: created mongodb database with set up

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
